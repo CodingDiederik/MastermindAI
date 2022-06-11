fcb119c7009123a7aca16d7084a2ae1ff1eeaace
CodingDiederik <diederikwebster@ziggo.nl>
2022-06-11 18:12:47 +0200

    Game af

--- main.py
+++ main.py
@@ -1,54 +1,50 @@
-import os.path
-import math
+class Mastermind():
+    def spel():
+        import bord
+        import Pionnen
+        import AI
 
-def main():
-    import bord
-    import Pionnen
-    import AI
+        running = True
+        random = 'ja'
 
-    running = True
-    random = 'ja'
-    Agent = AI.Agent()
+        #random = str(input('Wilt u de getallen willekeurig laten genereren? '))
+        if random == 'ja':
+            random = True
+        else:
+            random = False
 
-    #random = str(input('Wilt u de getallen willekeurig laten genereren? '))
-    if random == 'ja':
-        random = True
-    else:
-        random = False
+        pionnen = Pionnen.pion()
+        bord = bord.Bord()
 
-    pionnen = Pionnen.pion()
-    bord = bord.Bord()
+        if random == False:
+            pionnen.antwoord()
+        else:
+            pionnen.willekeurigantwoord()
 
-    if random == False:
-        pionnen.antwoord()
-    else:
-        pionnen.willekeurigantwoord()
+        #print('Instructie voor het raden: Er zijn 6 kleuren aanwezig: paars, oranje, geel, lichtgroen, blauw en rood.')
+        #print('Je raadt in één keer alle kleuren, dit doe je door de eerste letter van de kleur te noteren.')
+        #print('Je krijgt dan bijvoorbeeld l o g p, dit staat dan voor lichtgroen, oranje, geel en paars.\n')
 
-    #print('Instructie voor het raden: Er zijn 6 kleuren aanwezig: paars, oranje, geel, lichtgroen, blauw en rood.')
-    #print('Je raadt in één keer alle kleuren, dit doe je door de eerste letter van de kleur te noteren.')
-    #print('Je krijgt dan bijvoorbeeld l o g p, dit staat dan voor lichtgroen, oranje, geel en paars.\n')
+        while running == True:
 
-    while running == True:
+            geraad = pionnen.raden()
 
-        #geraad = pionnen.raden()
-        geraad = Agent.gok()
+            wit = pionnen.aantalWit(geraad)
+            zwart = pionnen.aantalZwart(geraad)
+            wit = zwart[1]
+            zwart = zwart[0]
 
-        wit = pionnen.aantalWit(geraad)
-        zwart = pionnen.aantalZwart(geraad)
-        wit = zwart[1]
-        zwart = zwart[0]
+            ronde = bord.tekenBord(geraad, wit, zwart)
 
-        ronde = bord.tekenBord(geraad, wit, zwart)
+            if zwart == 4:
+                #print('Je hebt de juiste kleurencombinatie geraden!')
+                beloning = 10
+                running = False
 
-        if zwart == 4:
-            #print('Je hebt de juiste kleurencombinatie geraden!')
-            running = False
+            if ronde == 11 and running == True:
+                running = False
+                beloning = -10
+                bord.tekenBord(geraad, wit, zwart)
+                #print('Je hebt helaas niet de juiste kleurencombinatie geraden')
 
-        if ronde == 11 and running == True:
-            running = False
-            bord.tekenBord(geraad, wit, zwart)
-            #print('Je hebt helaas niet de juiste kleurencombinatie geraden')
-
-
-if __name__ == '__main__':
-    main()
+Mastermind.spel()
